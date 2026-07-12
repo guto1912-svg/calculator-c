f1c31e29297347a832b7be3d13748673a84636d9
Gustavo <guto1912@usp.br>
2026-07-12 14:47:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Calculator in C
+
+A simple calculator developed in C to practice programming fundamentals.
+
+## Features
+
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- Factorial
+
+## Technologies
+
+- C
+- GCC
+- Visual Studio Code
+
+## How to compile
+
+```bash
+gcc src/calculator.c -o calculator.exe
+```
+
+## How to run
+
+```bash
+.\calculator.exe
+```
---
+++ src/calculator.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+float addition (float a, float b){
+    return a + b;
+}
+float subtraction (float a, float b){
+    return a - b;
+}
+float multiplication(float a, float b){
+    return a * b;
+}
+float division(float a, float b){
+    if (b == 0){
+        printf("Impossible to divide by zero!");
+        return 0;
+    }
+   return a/b; 
+}
+int factorial(int n){
+    int x = 1;
+    if (n<0){
+        printf("Invalid");
+        return -1;
+    }
+    while (n!=0){
+        x *= n;
+        n--;
+    }
+    return x;
+}       
+
+
+int main(){
+    int num, t;
+    int long factorial_result;
+    float x, y, result;
+    printf("1 -- Addition \n2 -- Subtraction \n3 -- Multiplication \n4 -- Division \n5 -- Factorial \n0 -- Exit \n");
+    printf("Select the operation: ");
+    scanf("%d", &num);
+    if (num<0 || num>5){
+        printf("Invalid option!");
+        return 0;
+    }
+    else if (num==1 || num==2 || num==3 || num==4){ 
+        printf("Write two numbers: ");
+        scanf("%f %f",&x, &y);
+    }
+    else {
+        printf("Write one number: ");
+        scanf("%d", &t);
+    }
+    switch (num){
+        case 0:
+            printf("See you next time!");
+            return 0;
+        case 1:
+           result = addition(x, y);
+            break;
+        case 2:
+            result = subtraction(x, y);
+            break;
+        case 3: 
+            result = multiplication(x, y);
+            break;
+        case 4:
+            result = division(x, y);
+            break;
+        case 5: 
+            result = factorial(t);
+            break;
+        default: 
+            printf("Invalid option!");
+            return 0;
+    }
+    printf("Result: %.2f\n", result);
+    return 0;
+}
